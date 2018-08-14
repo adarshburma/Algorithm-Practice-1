7f64adb9f0b8bc9d972f001b3eb21055b47b9a4c
adarshburma-iheartmedia <AdarshBurma@iheartmedia.com>
2018-08-14 14:09:09 -0500

    added methods to delete duplicates unsorted list

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,4 @@
-package com.ihm.automation.utils.uputils.fulfillment;
+import java.util.HashSet;
 
 public class LinkedList {
 
@@ -27,6 +27,22 @@ public class LinkedList {
         }
     }
 
+    public Node reversePractice(Node node){
+        Node prev = null;
+        Node next = null;
+        Node current = node;
+
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        node = prev;
+        return node;
+
+    }
     public Node reverse(Node node){
 
         Node current = node;
@@ -58,6 +74,78 @@ public class LinkedList {
         }
 
         return poped;
+    }
+
+    public void deleteNodeFromEnd(int n){
+        Node fast = head;
+        Node slow = head;
+        int fast_ctr = 0;
+        int slow_ctr = 0;
+
+        while(fast != null){
+            fast = fast.next;
+            fast_ctr++;
+        }
+
+        int offset = fast_ctr - n;
+        while (slow_ctr < offset){
+            if(slow_ctr == offset-1){
+                slow.next = slow.next.next;
+                break;
+            }
+            slow_ctr++;
+            slow = slow.next;
+        }
+    }
+
+    public void removeDuplicatesWithStorage(){
+        Node previous = head;
+        Node current = head.next;
+        HashSet<Integer> hash = new HashSet<>();
+        while(current != null){
+            if(hash.contains(current.data)){
+                previous.next = current.next;
+            }else{
+                hash.add(current.data);
+                previous = current;
+            }
+            current = current.next;
+        }
+    }
+
+    public void deleteNodeFromEnd2(int n){
+        Node fast = head;
+        Node slow = head;
+        int fast_ctr = 0;
+
+        while(fast_ctr < n+1){
+            fast = fast.next;
+            fast_ctr++;
+        }
+
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+    }
+
+    public void removeDuplicatesNoStorage(){
+        Node slow = head;
+        Node fast = null;
+
+        while(slow != null){
+            fast = slow;
+            while(fast.next != null){
+                if(fast.next.data == slow.data){
+                    fast.next = fast.next.next;
+                }else{
+                    fast = fast.next;
+                }
+            }
+            slow = slow.next;
+        }
     }
 
     public void printListFrom(Node node) {
