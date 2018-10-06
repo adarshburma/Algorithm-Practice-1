46b9dfe646fe2aba62db52d33b8cbf0492e1b50c
adarshburma-iheartmedia <35075578+adarshburma-iheartmedia@users.noreply.github.com>
2018-10-06 07:45:13 -0500

    updated solution 2

--- leetCode/ParanthesisMatching.java
+++ leetCode/ParanthesisMatching.java
@@ -37,4 +37,27 @@ class ParanthesisMatching{
         
         return false;
     }
+  
+     public boolean isValid2(String s) {
+        HashMap<Character, Character> map = new HashMap<Character,Character>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.keySet().contains(c)){
+                stack.push(c);
+            }
+            else if(map.values().contains(c)){
+                if(!stack.isEmpty() && map.get(stack.peek()) == c){
+                    stack.pop();
+                } else{
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
 }
