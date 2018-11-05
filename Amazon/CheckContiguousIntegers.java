700981e64e26e30a17ed2cb3ebbe092e2a27517b
adarshburma-iheartmedia <35075578+adarshburma-iheartmedia@users.noreply.github.com>
2018-11-05 13:50:58 -0600

    Update CheckContiguousIntegers.java

--- Amazon/CheckContiguousIntegers.java
+++ Amazon/CheckContiguousIntegers.java
@@ -1,6 +1,18 @@
 package org.practice.courses.courseapi;
 import java.util.HashSet;
 
+
+/*
+Check if array contains contiguous integers with duplicates allowed
+Given an array of n integers(duplicates allowed). Print “Yes” if it is a set of contiguous integers else print “No”. 
+Input : arr[] = {5, 2, 3, 6, 4, 4, 6, 6}
+Output : Yes
+The elements form a contiguous set of integers
+which is {2, 3, 4, 5, 6}.
+Input : arr[] = {10, 14, 10, 12, 12, 13, 15}
+Output : No
+*/
+
 public class CheckContiguousIntegers {
     static boolean checkContiguous(int[] arr){
         HashSet<Integer> set = new HashSet<>();
