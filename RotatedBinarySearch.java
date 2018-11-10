b68ebc5c4fe8147c7916f2500fbb9e30dd9ce1e6
adarshburma-iheartmedia <35075578+adarshburma-iheartmedia@users.noreply.github.com>
2018-11-09 20:07:21 -0600

    Update RotatedBinarySearch.java

--- RotatedBinarySearch.java
+++ RotatedBinarySearch.java
@@ -58,6 +58,37 @@ public class RotatedBinarySearch {
 
         return binarySearch(arr, mid+1, high, key);
     }
+    
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            
+             if(nums[left]<nums[mid]){
+                if(nums[left]<=target && target<nums[mid]){
+                    right=mid-1;
+                } else {
+                    left=mid+1;
+                }
+            }else if(nums[left]>nums[mid]){
+                if(nums[mid]<target && target<=nums[right]){
+                    left=mid+1;
+                } else {
+                    right=mid-1;
+                }
+            } else {
+                left++;
+            }    
+    
+        }
+        return -1;
+    }
+
 
     public static void main(String args[]){
         RotatedBinarySearch rotatedBinarySearch = new RotatedBinarySearch();
