4dd4a1804036dab09d5bc27f22b253747f81420d
Adarsh Burma <burma.adarsh@gmail.com>
2021-05-17 21:43:47 -0500

    Update MinDeletionsToMakeFrequencyOfEachLetterUnique.java

--- microsoft/MinDeletionsToMakeFrequencyOfEachLetterUnique.java
+++ microsoft/MinDeletionsToMakeFrequencyOfEachLetterUnique.java
@@ -1,29 +1,39 @@
 /* https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/ */
 
 public int minDeletions(String str) {
+        //map to hold original counts atmost O(26) constant space
         HashMap<Character, Integer> count = new HashMap<>();
+        
+        // minimum number of deletions
         int deletions = 0;
+        
+        // set contains only unique counts
         HashSet<Integer> set = new HashSet<>();
         
+        // capture originak counts
         for(char c : str.toCharArray()) {
-            if(count.containsKey(c)) {
-                count.put(c, count.get(c)+1);
-            } else {
-                count.put(c, 1);
-            }
+             count.put(c, count.getOrDefault(c, 0)+1);
         }
-
+        
+       // loop through entries 
         for(Map.Entry<Character,Integer> e : count.entrySet()) {
             int val = e.getValue();
+            
+            // delete character until its count doesn't match any counts until then
             while(val > 0 && set.contains(val)) {
                 deletions++;
                 val--;
             }
             
+            // add count val only if its > 0
             if(val > 0) {
                 set.add(val);
             }
         }
+        
+        // return deletions
         return deletions;
     }
-}
+    
+    // Time : O(len(s))
+    // Space: O(26) - constant
